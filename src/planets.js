64a953af205c619367626bf6a7749e0382226bdd
sagepaden <sagepaden@gmail.com>
2023-06-25 15:28:54 -0700

    code has been built, testing to see now that it passes

--- src/planets.js
+++ src/planets.js
@@ -2,9 +2,13 @@ export default class Human {
     constructor(baseEarthAge) {
       this.baseEarthAge = baseEarthAge;
       this.yearsFrom = 0;
+      this.yearsTil = 0;
     }
 
     earthYearsFrom(pastAge) {
       this.yearsFrom = this.baseEarthAge - pastAge;
+    }
+    earthYearsTil(futureAge) {
+      this.yearsTil = futureAge - this.baseEarthAge;
     }
 };
