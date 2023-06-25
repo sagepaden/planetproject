f28b82a4e4419f9ea372bc01c9d402fc09e9e8ab
sagepaden <sagepaden@gmail.com>
2023-06-25 15:26:49 -0700

    test has failed

--- __tests__/planets.test.js
+++ __tests__/planets.test.js
@@ -11,8 +11,14 @@ import Human from '../src/planets.js';
         expect(human.baseEarthAge).toEqual(56);
     });
 
-    test('should return a method that tells you how many years from an age you are at your current age', () => {
+    test('should return a method that tells you how many years from a past age you are at your current age', () => {
         human.earthYearsFrom(43)
         expect(human.yearsFrom).toEqual(13);
     });
+
+    test('should return a method that tells you how many years from a future age you are at your current age', () => {
+        human.earthYearsTil(61)
+        expect(human.yearsTil).toEqual(5);
+    });
+
 })
