bc6c3b925cce4df08d05f45f1603666d8ed58b47
sagepaden <sagepaden@gmail.com>
2023-06-25 15:21:26 -0700

    test has passed after spelling corrected, moving to next test

--- __tests__/planets.test.js
+++ __tests__/planets.test.js
@@ -10,4 +10,9 @@ import Human from '../src/planets.js';
     test('should create a "Human" object with the age of 56', () => {
         expect(human.baseEarthAge).toEqual(56);
     });
+
+    test('should return a method that tells you how many years from an age you are at your current age', () => {
+        human.earthYearsFrom(46)
+        expect(human.yearsFrom).toEqual(13);
+    });
 })
--- src/planets.js
+++ src/planets.js
@@ -1,5 +1,10 @@
 export default class Human {
     constructor(baseEarthAge) {
       this.baseEarthAge = baseEarthAge;
+      this.yearsFrom = 0;
+    }
+
+    earthYearsFrom(pastAge) {
+      this.yearsFrom = this.baseEarthAge - pastAge;
     }
 };
