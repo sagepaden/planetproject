6019e1ed140234f162ab133ad5819e0045df3d6b
sagepaden <sagepaden@gmail.com>
2023-06-25 23:17:58 -0700

    finishing touches and a README added.

---
+++ README.md
@@ -0,0 +1,60 @@
+# _Planets_
+
+#### _This application converts your age from Earth years to Mercury, Venus, Mars, and Jupiter years_
+
+#### By **Sage Paden**
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _JavaScript_
+* _Webpack_
+* _Node.js_
+* _esLint_
+* _Babel_
+
+## Description
+
+_This application demonstrates Red-Green-Refactor workflow for testing via Jest on the local terminal. Intermediate JavaScript skills showcasing ES6 class keywords for constructors. Application displays 100% line coverage via Jest._
+
+## Setup/Installation Requirements
+
+* _Clone “planets“ from the repository to your desktop_
+* _Navigate to “planets“ directory via your local terminal command line_
+* _Open the directory in VS code by typing 'code .' in the command line_
+* _Run 'npm install' in the command line to install all packages_
+* _Run 'npm run build' to build the project via webpack_
+* _Run 'npm run start' to start a development server_
+* _Run 'npm run lint' to lint all JS files_
+* _Run 'npm run test' to test files via Jest_
+
+## Known Bugs
+
+* _No known bugs as of 06/25/23_
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [Sage Paden]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+_If you run into any issues or have questions, ideas or concerns, please reach out to me via email: sagepaden@gmail.com.  Contributions to the code are highly encouraged._
--- src/planets.js
+++ src/planets.js
@@ -1,94 +1,99 @@
 export default class Human {
-    constructor(baseEarthAge) {
-      this.baseEarthAge = baseEarthAge;
-      this.earthYearsFrom = 0;
-      this.earthYearsTil = 0;
+  constructor(baseEarthAge) {
+    this.baseEarthAge = baseEarthAge;
+    this.earthYearsFrom = 0;
+    this.earthYearsTil = 0;
 
-      this.baseMercuryYears = 0;
-      this.mercuryYearsFrom = 0;
-      this.mercuryYearsTil = 0;
+    this.baseMercuryYears = 0;
+    this.mercuryYearsFrom = 0;
+    this.mercuryYearsTil = 0;
 
-      this.baseVenusYears = 0;
-      this.venusYearsFrom = 0;
-      this.venusYearsTil = 0;
+    this.baseVenusYears = 0;
+    this.venusYearsFrom = 0;
+    this.venusYearsTil = 0;
 
-      this.baseMarsYears = 0;
-      this.marsYearsFrom = 0;
-      this.marsYearsTil = 0;
+    this.baseMarsYears = 0;
+    this.marsYearsFrom = 0;
+    this.marsYearsTil = 0;
 
-      this.baseJupiterYears = 0;
-      this.jupiterYearsFrom = 0;
-      this.jupiterYearsTil = 0;
-    }
+    this.baseJupiterYears = 0;
+    this.jupiterYearsFrom = 0;
+    this.jupiterYearsTil = 0;
+  }
 
-    fixMyNum(number) {
-      return Math.trunc(number*100)/100;
-    }
+  fixMyNum(number) {
+    return Math.trunc(number*100)/100;
+  }
 
-    mathEarthYearsFrom(pastAge) {
-      this.earthYearsFrom = this.baseEarthAge - pastAge;
-    }
+  mathEarthYearsFrom(pastAge) {
+    this.earthYearsFrom = this.baseEarthAge - pastAge;
+  }
 
-    mathEarthYearsTil(futureAge) {
-      this.earthYearsTil = futureAge - this.baseEarthAge;
-    }
-
-
-    mercury() {
-      let mercYLong = Number(this.baseEarthAge / .24);
-      this.baseMercuryYears = this.fixMyNum(mercYLong);
-    }
-
-    mathMercuryYearsFrom(pastAge) {
-      let mercYFLong = Number((this.baseEarthAge - pastAge) / .24);
-      this.mercuryYearsFrom = this.fixMyNum(mercYFLong);
-    }
-
-    mathMercuryYearsTil(futureAge) {
-      let mercYTLong = Number((futureAge - this.baseEarthAge) / .24);
-      this.mercuryYearsTil = this.fixMyNum(mercYTLong);
-    }
+  mathEarthYearsTil(futureAge) {
+    this.earthYearsTil = futureAge - this.baseEarthAge;
+  }
 
 
-    venus() {
-      let venusYLong = Number(this.baseEarthAge / .62);
-      this.baseVenusYears = this.fixMyNum(venusYLong);
-    }
+  mercury() {
+    let mercYLong = Number(this.baseEarthAge / .24);
+    this.baseMercuryYears = this.fixMyNum(mercYLong);
+  }
 
-    mathVenusYearsFrom(pastAge) {
-      let venYFLong = Number((this.baseEarthAge - pastAge) / .62);
-      this.venusYearsFrom = this.fixMyNum(venYFLong);
-    }
+  mathMercuryYearsFrom(pastAge) {
+    let mercYFLong = Number((this.baseEarthAge - pastAge) / .24);
+    this.mercuryYearsFrom = this.fixMyNum(mercYFLong);
+  }
 
-    mathVenusYearsTil(futureAge) {
-      let venYTLong = Number((futureAge - this.baseEarthAge) / .62);
-      this.venusYearsTil = this.fixMyNum(venYTLong);
-    }
-
-
-    mars() {
-      let marsYLong = Number(this.baseEarthAge / 1.88);
-      this.baseMarsYears = this.fixMyNum(marsYLong);
-    }
-
-    mathMarsYearsFrom(pastAge) {
-      let marsYFLong = Number((this.baseEarthAge - pastAge) / 1.88);
-      this.marsYearsFrom = this.fixMyNum(marsYFLong);
-    }
-
-    mathMarsYearsTil(futureAge) {
-      let marsYTLong = Number((futureAge - this.baseEarthAge) / 1.88);
-      this.marsYearsTil = this.fixMyNum(marsYTLong);
-    }
+  mathMercuryYearsTil(futureAge) {
+    let mercYTLong = Number((futureAge - this.baseEarthAge) / .24);
+    this.mercuryYearsTil = this.fixMyNum(mercYTLong);
+  }
 
 
-    jupiter() {
-      let jupYLong = Number(this.baseEarthAge / 11.86);
-      this.baseJupiterYears = this.fixMyNum(jupYLong);
-    }
+  venus() {
+    let venusYLong = Number(this.baseEarthAge / .62);
+    this.baseVenusYears = this.fixMyNum(venusYLong);
+  }
 
-    mathJupiterYearsFrom(pastAge) {
-      let jupYFLong = Number((this.baseEarthAge - pastAge) / 11.86);
-      this.jupiterYearsFrom = this.fixMyNum(jupYFLong);
-    }
-};
+  mathVenusYearsFrom(pastAge) {
+    let venYFLong = Number((this.baseEarthAge - pastAge) / .62);
+    this.venusYearsFrom = this.fixMyNum(venYFLong);
+  }
+
+  mathVenusYearsTil(futureAge) {
+    let venYTLong = Number((futureAge - this.baseEarthAge) / .62);
+    this.venusYearsTil = this.fixMyNum(venYTLong);
+  }
+
+
+  mars() {
+    let marsYLong = Number(this.baseEarthAge / 1.88);
+    this.baseMarsYears = this.fixMyNum(marsYLong);
+  }
+
+  mathMarsYearsFrom(pastAge) {
+    let marsYFLong = Number((this.baseEarthAge - pastAge) / 1.88);
+    this.marsYearsFrom = this.fixMyNum(marsYFLong);
+  }
+
+  mathMarsYearsTil(futureAge) {
+    let marsYTLong = Number((futureAge - this.baseEarthAge) / 1.88);
+    this.marsYearsTil = this.fixMyNum(marsYTLong);
+  }
+
+
+  jupiter() {
+    let jupYLong = Number(this.baseEarthAge / 11.86);
+    this.baseJupiterYears = this.fixMyNum(jupYLong);
+  }
+
+  mathJupiterYearsFrom(pastAge) {
+    let jupYFLong = Number((this.baseEarthAge - pastAge) / 11.86);
+    this.jupiterYearsFrom = this.fixMyNum(jupYFLong);
+  }
+
+  mathJupiterYearsTil(futureAge) {
+    let jupYTLong = Number((futureAge - this.baseEarthAge) / 11.86);
+    this.jupiterYearsTil = this.fixMyNum(jupYTLong);
+  }
+}
